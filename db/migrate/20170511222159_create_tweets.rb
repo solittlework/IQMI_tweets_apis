619f074239aeb98d9c8a3fb8f4bd93fe05eed1e4
Haipeng Li <haipeng3@ualberta.ca>
2017-05-11 18:39:04 -0400

    add initial columns to tweet table

--- db/migrate/20170511222159_create_tweets.rb
+++ db/migrate/20170511222159_create_tweets.rb
@@ -1,6 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets do |t|
+      t.integer "followers" 
+      t.text "message"
+      t.decimal "sentiment", :precision => 3, :scale => 1
+      t.string "user_handle"
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170511222159) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer  "followers"
+    t.text     "message"
+    t.decimal  "sentiment",   precision: 3, scale: 1
+    t.string   "user_handle"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
 
 end
